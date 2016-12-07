9ded5fda9135f71a5570d91e2143ca3a1feb3501
miguel <fuermiguel@yahoo.es>
2016-12-07 22:40:31 +0000

    hasta pag 109 essensial

---
+++ __openerp__.py
@@ -0,0 +1,8 @@
+{
+'name': 'To-Do Application',
+'description': 'Manage your personal Tasks with this module.',
+'author': 'Daniel Reis',
+'depends': ['partes'],
+'application': True,
+'data': ['views/todo_view.xml'],
+}
